0de8ab2057a1770af533e272ddf64cb660555fad
Lorena Peñas <lorena.penas@alumnos.upm.es>
2025-12-08 12:55:45 +0100

    Linter applied

--- app/main.py
+++ app/main.py
@@ -4,20 +4,10 @@ from fastapi.responses import RedirectResponse
 from app.application.get_dice_roll import get_dice_roll
 
 
-
-
-
-
-
-
-
-
-
-
 def create_app() -> FastAPI:
     app = FastAPI()
 
-    app.add_api_route(path="/", endpoint=lambda: RedirectResponse(app.url_path_for(get_dice_roll.__name__)), methods=["GET"], include_in_schema=False,)
+    app.add_api_route(path="/", endpoint=lambda: RedirectResponse(app.url_path_for(get_dice_roll.__name__)), methods=["GET"], include_in_schema=False)
 
     app.add_api_route(
         path="/dice/roll",
