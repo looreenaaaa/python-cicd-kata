069243f02da7a423d28e9f54d37c8f316f591e3b
Lorena Peñas <lorena.penas@alumnos.upm.es>
2025-12-08 12:59:20 +0100

    Format applied

--- app/main.py
+++ app/main.py
@@ -9,10 +9,7 @@ def create_app() -> FastAPI:
 
     app.add_api_route(path="/", endpoint=lambda: RedirectResponse(app.url_path_for(get_dice_roll.__name__)), methods=["GET"], include_in_schema=False)
 
-    app.add_api_route(
-        path="/dice/roll",
-        endpoint=get_dice_roll
-    )
+    app.add_api_route(path="/dice/roll", endpoint=get_dice_roll)
 
     return app
 
